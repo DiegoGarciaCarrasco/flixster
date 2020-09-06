3852dac7f815536c23be18a90989beca9a58340e
Diego Garcia <diegoalonzo_prod@hotmail.com>
2020-09-06 03:36:36 -0700

    Initial Commit

---
+++ flixster/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  flixster
+//
+//  Created by Diego Garcia on 9/6/20.
+//  Copyright © 2020 garciadiegoTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
