6abfdb8dee4a8d0637a096344c2f3c1d2af9312f
Diego Garcia <diegoalonzo_prod@hotmail.com>
2020-09-10 23:40:22 -0700

    Unit 2 - Assignment Update

---
+++ flixster/MovieGridCell.swift
@@ -0,0 +1,15 @@
+//
+//  MovieGridCell.swift
+//  flixster
+//
+//  Created by Diego Garcia on 9/10/20.
+//  Copyright © 2020 garciadiegoTeam. All rights reserved.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+    
+}
